7bc650efcd96ed7a3030ab95e30ce90059b147d9
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-06-29 22:59:58 +0530

    test(auth): add registration API tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RegisterAPITest(APITestCase):
+
+    def test_user_registration(self):
+
+        url = "/api/register/"
+
+        data = {
+            "username": "navaneeth",
+            "email": "nav@example.com",
+            "password": "password123",
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED,
+        )
+
+        self.assertEqual(
+            User.objects.count(),
+            1,
+        )
+
+        self.assertEqual(
+            User.objects.get().username,
+            "navaneeth",
+        )
+
+        self.assertTrue(
+            User.objects.get().check_password("password123")
+        )
