180ef6259343c4b7857ef341dee14f2d677f4402
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-06-29 22:50:29 +0530

    feat(auth): add user registration API

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(
+            {
+                "message": "User created successfully"
+            },
+            status=status.HTTP_201_CREATED,
+        )
--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
     'tasks',
 ]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
