77738104b86fe5102e866a483bba15165998c81a
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-06-30 11:38:49 +0530

    feat(auth): add JWT login endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import LoginView, RegisterView
 
 urlpatterns = [
     path(
         "register/",
         RegisterView.as_view(),
         name="register",
+    ),
+
+    path(
+        "login/",
+        LoginView.as_view(),
+        name="login",
     ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .serializers import RegisterSerializer
 
 
@@ -23,3 +25,7 @@ class RegisterView(CreateAPIView):
             },
             status=status.HTTP_201_CREATED,
         )
+
+
+class LoginView(TokenObtainPairView):
+    pass
